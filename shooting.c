0296cf29e11c8541f0044e86cd89fcd2d5f11a7b
abostrom8 <abostrom8@users.noreply.github.com>
2015-03-19 23:22:27 -0400

    Can now read in energies from file

--- shooting.c
+++ shooting.c
@@ -3,8 +3,10 @@
 #include <math.h>
 
 #define v 140
-//#define E -0.0882721 /* E/V0 */
 #define out_file "output.dat"
+#define num_evals 10
+
+double E[num_evals]
 
 void euler(double u[], double dudt[], double h, int theta, double E){
     dudt[0]=u[1];
@@ -17,10 +19,7 @@ void euler(double u[], double dudt[], double h, int theta, double E){
 int main(int argc, char *argv[]){
     double h=0.0001,E;
     double u[2],dudt[2];
-    int i,theta;
-    
-    // Read in user-supplied energy
-    E=atoi(argv[1]);
+    int i,j,theta;
     
     // Initial conditions
     u[0]=0.0;
@@ -32,17 +31,32 @@ int main(int argc, char *argv[]){
     // Print initial conditions to output file
     fprintf(fp,"%lf %lf\n",0.0,u[0]);
     
-    for(i=1;i<2e4;i++) {
-        /* Step function */
-        if(i*h<=1.0){
-            theta=1.0;
+    // Create a pointer to input file
+    FILE *infile=fopen(argv[1],"r");
+    // Error message if no file specified
+    if (infile==NULL){
+        printf("\nUsage: %s filename\n",argv[0]);
+        return 0;
+    }
+    //Read input file into array of E values
+    for(i=;i<num_evals;i++){
+        fscanf(infile,"%f",&E[i]);
+    }
+    
+    for(j=0;j<num_evals;j++){
+        for(i=1;i<2e4;i++) {
+            /* Step function */
+            if(i*h<=1.0){
+                theta=1.0;
+            }
+            if(i*h>1.0){
+                theta=0.0;
+            }
+            E=E[j];
+            // Integrate
+            euler(u,dudt,h,theta,E);
+            // Print results to output file as r/a u
+            fprintf(fp,"%lf %lf\n",i*h,u[0]);
         }
-        if(i*h>1.0){
-            theta=0.0;
-        }
-        // Integrate
-        euler(u,dudt,h,theta,E);
-        // Print results to output file as r/a u
-        fprintf(fp,"%lf %lf\n",i*h,u[0]);
     }
 }
